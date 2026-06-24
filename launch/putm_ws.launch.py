35a763bae93438d3a329c0ab7fcfe0d4a36db6b8
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2026-06-24 12:26:43 +0200

    Launch refactor: putm_ws.launch.py was refactored to be more modular

--- launch/putm_ws.launch.py
+++ launch/putm_ws.launch.py
@@ -2,46 +2,31 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
-
 from launch_ros.substitutions import FindPackageShare
 
-
 def generate_launch_description():
-
-    return LaunchDescription(
-        [
+    packages_to_launch = [
+        ("putm_vcl", None),
+        ("putm_controller", None),
+        ("putm_lap_timer", None),
+        ("xsens_mti_ros2_driver", "xsens_mti_node.launch.py"),
+    ]
+    
+    ld = LaunchDescription()
+    
+    for pkg, launch_file in packages_to_launch:
+        actual_launch_file = launch_file if launch_file else f"{pkg}.launch.py"
+        
+        ld.add_action(
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    PathJoinSubstitution(
-                        [
-                            FindPackageShare("putm_vcl"),
-                            "launch",
-                            "putm_vcl.launch.py",
-                        ]
-                    )
-                )
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    PathJoinSubstitution(
-                        [
-                            FindPackageShare("putm_controller"),
-                            "launch",
-                            "putm_controller.launch.py",
-                        ]
-                    )
-                )
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    PathJoinSubstitution(
-                        [
-                            FindPackageShare("putm_lap_timer"),
-                            "launch",
-                            "putm_lap_timer.launch.py",
-                        ]
-                    )
+                    PathJoinSubstitution([
+                        FindPackageShare(pkg),
+                        "launch",
+                        actual_launch_file
+                    ])
                 )
             )
-        ]
-    )
+        )
+        
+    return ld
